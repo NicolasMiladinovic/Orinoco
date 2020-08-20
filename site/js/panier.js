42177eddb4e734d6ce4dfd177b13c030765e7f44
NicolasMiladinovic <62346327+NicolasMiladinovic@users.noreply.github.com>
2020-08-20 22:53:29 +0200

    Avancement Panier

--- site/js/panier.js
+++ site/js/panier.js
@@ -12,8 +12,20 @@ let panier = document.getElementById('produit');
 itemList.forEach((item) => {
     console.log(item.id + ' ' + item.name + ' ' + item.price)
 
-    let newDiv = document.createElement("div");
-    newDiv.textContent = item.name + ' ' + item.price;
-    panier.appendChild(newDiv);
-});
+    let newDivImg = document.createElement("div");
+    newDivImg.textContent = item.image;
+    newDivImg.setAttribute('id' , 'divImg');
+
+    let newDivName = document.createElement('div');
+    newDivName.textContent = item.name;
+    newDivName.setAttribute('id' , 'divName');
+
+    let newDivPrice = document.createElement('div');
+    newDivPrice.textContent = item.price;
+    newDivPrice.setAttribute('id' , 'divPrice');
+
+    panier.appendChild(newDivImg);
+    panier.appendChild(newDivName);
+    panier.appendChild(newDivPrice);
+    });
 
--- site/js/produit.js
+++ site/js/produit.js
@@ -1,47 +1,46 @@
-console.log('produit');
+//appelle tous les boutons des teddys
 
-const teddys_div = document.getElementById('teddys');
+// const teddys_div = document.getElementById('teddys');
 
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
-teddys_div.appendChild(container);
+// const container = document.createElement('div');
+// container.setAttribute('class', 'container');
+// teddys_div.appendChild(container);
 
-let request = new XMLHttpRequest();
-request.open('GET', 'http://localhost:3000/api/teddies', true);
-request.onload = function () {
-    let data = JSON.parse(this.response);
-    if (request.status >= 200 && request.status < 400) {
-        data.forEach((teddy) => {
+// let request = new XMLHttpRequest();
+// request.open('GET', 'http://localhost:3000/api/teddies', true);
+// request.onload = function () {
+//     let data = JSON.parse(this.response);
+//     if (request.status >= 200 && request.status < 400) {
+//         data.forEach((teddy) => {
+//             const card = document.createElement('div');
+//             card.setAttribute('class', 'card');
+
+//             const button = document.createElement('button');
+//             button.setAttribute("value", teddy._id);
+//             button.setAttribute("class", "addCartButton");
+//             button.textContent = "Ajouter au panier";
+//             button.addEventListener("click", function (e) {              
+//                 let actualCart = localStorage.getItem("cart");
+//                 let actualCartInJson=[];
+//                 if (actualCart!=null){  
+//                      actualCartInJson = JSON.parse(actualCart);
+//                     //  console.log(actualCartInJson[0].id);
+//                 }
+//                 let newElement = {id: teddy._id, name: teddy.name, price: teddy.price, image: teddy.imageUrl};
+//                 actualCartInJson.push(newElement);
+//                 localStorage.setItem("cart", JSON.stringify(actualCartInJson));
+//                 console.log(actualCartInJson);
+//             });
             
-        console.log('request');
-        
-            const card = document.createElement('div');
-            card.setAttribute('class', 'card');
+//             container.appendChild(card);
+          
+//             card.appendChild(button);
+//         });
+//     } else {
+//         const errorMessage = document.createElement('marquee');
+//         errorMessage.textContent = "It's not working !";
+//         teddys_div.appendChild(errorMessage);
+//     };
+// };
 
-            const button = document.createElement('button');
-            button.setAttribute("value", teddy._id);
-            button.setAttribute("class", "addCartButton");
-            button.textContent = "Ajouter au panier";
-            button.addEventListener("click", function (e) {
-                let actualCart = localStorage.getItem("cart");
-                let actualCartInJson = [];
-                if (actualCart != null) {
-                    actualCartInJson = JSON.parse(actualCart);
-                    //  console.log(actualCartInJson[0].id);
-                }
-                let newElement = { id: teddy._id, name: teddy.name, price: teddy.price, image: teddy.imageUrl };
-                actualCartInJson.push(newElement);
-                localStorage.setItem("cart", JSON.stringify(actualCartInJson));
-                console.log(actualCartInJson);
-            });
-
-            card.appendChild(button);
-        });
-    } else {
-        const errorMessage = document.createElement('marquee');
-        errorMessage.textContent = "It's not working !";
-        teddys_div.appendChild(errorMessage);
-    };
-};
-
-request.send();
+// request.send();
