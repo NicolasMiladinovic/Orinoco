18f5ea6b7c4cb6afd074f2fedcc39021b087e6f8
NicolasMiladinovic <62346327+NicolasMiladinovic@users.noreply.github.com>
2020-09-14 23:39:20 +0200

    Avancement Panier (remove button fonctionnel)

--- site/js/panier.js
+++ site/js/panier.js
@@ -1,99 +1,84 @@
-// document.getElementById('produit').innerHTML = localStorage.getItem('cart');
+displayAllCart();
 
-let cartContent = localStorage.getItem('cart');
-let itemList = JSON.parse(cartContent);
+function displayAllCart() {
 
-console.log(itemList);
-
-// console.log(itemList[0].name);
-
-let totalPrice = 0;
-
-let panier = document.getElementById('produit');
-
-itemList.forEach((item) => {
-    console.log(item.id + ' ' + item.name + ' ' + item.price)
-
-    let newProduct = document.createElement('div');
-    newProduct.setAttribute('class', 'newProduct');
-
-    const newImg = document.createElement("img");
-    newImg.setAttribute('src', item.image);
-    newImg.setAttribute('id', 'divImg');
-
-    let newDivName = document.createElement('div');
-    newDivName.textContent = item.name;
-    newDivName.setAttribute('id', 'divName');
-
-    let newDivPrice = document.createElement('div');
-    newDivPrice.textContent = item.price;
-    totalPrice += item.price;
-    newDivPrice.setAttribute('id', 'divPrice');
-
-    newProduct.appendChild(newImg);
-    newProduct.appendChild(newDivName);
-    newProduct.appendChild(newDivPrice);
-
-    panier.appendChild(newProduct);
-});
+    let cartContent = localStorage.getItem('cart');
+    let itemList = JSON.parse(cartContent);
 
 
-document.getElementById('totalPrice').innerHTML = totalPrice;
+    let removeButton;
 
-console.log(totalPrice);
+    console.log(itemList);
+
+    // console.log(itemList[0].name);
+
+    let totalPrice = 0;
+
+    let panier = document.getElementById('produit');
+
+    itemList.forEach((item) => {
+        console.log(item.id + ' ' + item.name + ' ' + item.price)
+
+        let newProduct = document.createElement('div');
+        newProduct.setAttribute('class', 'newProduct');
+
+        removeButton = document.createElement('button');
+        removeButton.setAttribute('type', 'button');
+        removeButton.setAttribute('class', 'close');
+        removeButton.setAttribute('aria-label', 'Close');
+
+        let optionRemoveButton = document.createElement('span');
+        optionRemoveButton.setAttribute('aria-hidden', 'true');
+        optionRemoveButton.textContent = 'x';
 
 
+        removeButton.addEventListener('click', function () {
+            let indexCount = 0;
+            let mycart = JSON.parse(localStorage.getItem('cart'));
+            let clickedId = item.id;
+            console.log(clickedId);
+            console.log(panier);
+            mycart.forEach(function (data) {
+                if (data.id === clickedId) {
+                    mycart.splice(indexCount, 1);
+                    continue;
+                }
+                indexCount += 1;
+            })
 
-contact = {
-    nom: 'toto',
-    firstname: 'titi',
-    mail: 'tototiti',
-    adresse: 'Champs Elysées',
-};
+            localStorage.setItem('cart', JSON.stringify(mycart));
+            panier.innerHTML = "";
+            displayAllCart();
+        });
 
-product = [
-    'id1', 'id2'
-];
+        const newImg = document.createElement("img");
+        newImg.setAttribute('src', item.image);
+        newImg.setAttribute('id', 'divImg');
 
-// const panier = [
-//     {
-//       id: 11789,
-//       name: 'Norbert'
-//     },
-//     {
-//       id: 6789,
-//       name: "Nico"
-//     },
-//     {
-//       id: 1235641789,
-//       name: "Toni"
-//     }
-//   ]
-  
-  
-//   clickedId = 123456789;
-//   indexCount = 0;
-  
-//   console.log(panier)
-//   panier.forEach(function(oneItem){
-//       if (oneItem.id === clickedId){
-//         panier.splice(indexCount, 1)
-//       }
-//         indexCount += 1;
-//   })
-//   JSON.encode(panier);
-//   localStorage.setItem(cart, panier);
-//   var container = document.getElementById("Container");
-//   container.innerHTML = "";
-//   DisplayAllCart()
-  
-  
-  
-//   function DisplayAllCart(){
-//     var cart = localStorage.getItem("cart")
-//     var container = document.getElementById("Container")
-//     .......
-    
-    
-//     container.appendChild(AllContent)
-//   }
+        let newDivName = document.createElement('div');
+        newDivName.textContent = item.name;
+        newDivName.setAttribute('id', 'divName');
+
+        let newDivPrice = document.createElement('div');
+        newDivPrice.textContent = item.price;
+        totalPrice += item.price;
+        newDivPrice.setAttribute('id', 'divPrice');
+
+
+        removeButton.appendChild(optionRemoveButton);
+
+        newProduct.appendChild(removeButton);
+        newProduct.appendChild(newImg);
+        newProduct.appendChild(newDivName);
+        newProduct.appendChild(newDivPrice);
+
+        panier.appendChild(newProduct);
+
+        
+    });
+
+    console.log(totalPrice);
+
+    document.getElementById('totalPrice').innerHTML = totalPrice;
+
+}
