453498fb6fc914ffa3effc35a4bbbcbcc29a64bc
NicolasMiladinovic <62346327+NicolasMiladinovic@users.noreply.github.com>
2020-09-16 22:57:01 +0200

    Avancement Panier (début requête POST)

--- site/js/panier.js
+++ site/js/panier.js
@@ -97,3 +97,23 @@ function displayAllCart() {
     document.getElementById('totalPrice').innerHTML = totalPrice;
 
 }
+
+
+/////////////////////////
+// Forme requête POST //
+///////////////////////
+
+// var xhr = new XMLHttpRequest();
+// xhr.open("POST", '/server', true);
+
+// //Envoie les informations du header adaptées avec la requête
+// xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+// xhr.onreadystatechange = function() { //Appelle une fonction au changement d'état.
+//     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+// // Requête finie, traitement ici.
+//     }
+// }
+// xhr.send("foo=bar&lorem=ipsum");
+// // xhr.send(new Int8Array()); 
+// // xhr.send(document);
